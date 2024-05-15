f54a7e87552aa6fb8bc1dc78ad1e992aab1908d8
Nydragon <6582485n@proton.me>
2024-05-16 04:12:42 +0900

    feat: add some thunderbird settings

--- home/thunderbird/default.nix
+++ home/thunderbird/default.nix
@@ -2,7 +2,12 @@
 {
   programs.thunderbird = {
     enable = true;
-    #settings = { mailnews.wraplength = 80; };
+    settings = {
+      "mailnews.wraplength" = 80;
+      "mail.pane_config.dynamic" = 2;
+      "mail.biff.play_sound" = false;
+      "mail.chat.play_sound" = false;
+    };
     profiles.nico = {
       isDefault = true;
     };
