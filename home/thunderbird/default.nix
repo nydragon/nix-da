2d9b0bf51950fda5b434a1e2e0922d5d82d3a30f
Nydragon <6582485n@proton.me>
2024-05-16 12:28:07 +0900

    thunderbird: add more options

--- home/thunderbird/default.nix
+++ home/thunderbird/default.nix
@@ -7,6 +7,11 @@
       "mail.pane_config.dynamic" = 2;
       "mail.biff.play_sound" = false;
       "mail.chat.play_sound" = false;
+      # Never allow cookies
+      "network.cookie.cookieBehavior" = 2;
+      "mailnews.start_page.enabled" = false;
+      # Date descending
+      "mailnews.default_sort_order" = 2;
     };
     profiles.nico = {
       isDefault = true;
