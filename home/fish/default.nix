2efc22b67ee4768f8ff99aabb8d2deba6a1c6b96
Nydragon <contact@ccnlc.eu>
2024-05-27 23:02:14 +0900

    feat: add rebuild function

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -13,20 +13,17 @@
       mkdir = "mkdir --parent";
       cp = "cp -i";
     };
-    functions = {
-      gitignore = "curl -sL https://www.gitignore.io/api/$argv";
-      nrun = "nix run nixpkgs#$argv[1] -- $argv[2..]";
-      mv-bad-creation-date = ''exiftool -if 'not $CreateDate' -p '$FileName' "$PWD/$argv[1]" | xargs -I {} mv -i "$PWD/$argv[1]/{}" "$argv[2]"'';
-      rename-images = ''
-        set -f input "$PWD/$argv[1]"
-
-        if not test -d $input
-        echo "input is not a directory"
-        else
-        exiftool -if '$CreateDate' -p '$FileName' "$input" | xargs -I {} jhead -n%Y-%m-%d-%H%M%S "$input/{}"
-        end
-      '';
-      fish_prompt = builtins.readFile ./fish_prompt.fish;
-    };
+    functions =
+      let
+        rf = builtins.readFile;
+      in
+      {
+        gitignore = "curl -sL https://www.gitignore.io/api/$argv";
+        nrun = "nix run nixpkgs#$argv[1] -- $argv[2..]";
+        mv-bad-creation-date = ''exiftool -if 'not $CreateDate' -p '$FileName' "$PWD/$argv[1]" | xargs -I {} mv -i "$PWD/$argv[1]/{}" "$argv[2]"'';
+        rename-images = rf ./rename_images.fish;
+        fish_prompt = rf ./fish_prompt.fish;
+        rebuild = "env --chdir $HOME/.nixconf sudo nixos-rebuild switch --flake .#$(hostname)";
+      };
   };
 }
