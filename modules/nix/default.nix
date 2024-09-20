54a8b963775deafbe8c038f1466a6f377507de68
Nydragon <contact@ccnlc.eu>
2024-09-21 01:29:44 +0200

    feat: add cachix for hyprland

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,6 +1,5 @@
 { ... }:
 {
-
   imports = [ ./overlays.nix ];
 
   nixpkgs.config = {
@@ -20,6 +19,8 @@
       automatic = true;
     };
     settings = {
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       experimental-features = [
         "nix-command"
         "flakes"
